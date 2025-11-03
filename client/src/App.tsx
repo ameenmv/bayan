ee7e5d6a6e9e6075cfb6916b7f644da0ddede7f6
ameenmv <shehaaa4@gmai.com>
2025-11-03 17:39:21 +0200

    fix routes bug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,22 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+import { Route } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { queryClient } from "./lib/queryClient";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/toaster";
 import About from "@/pages/about";
 import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={About} />
-      <Route component={NotFound} />
-    </Switch>
+    <>
+      <Route path="/">
+        <About />
+      </Route>
+
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </>
   );
 }
 
